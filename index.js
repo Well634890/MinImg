4cf90c8c226a0715dfefa520be06a3fc0e708fa6
Well <well634890@126.com>
2020-03-16 14:26:54 +0800

    require.context() 说明

--- index.js
+++ index.js
@@ -4,6 +4,12 @@
  * E-mail: well634890@126.com
  */
 
+/* require.context(directory, useSubdirectories, regExp)
+* @param {String} directory 搜索的目录
+* @param { useSubdirectories} 是否搜索其子目录，可选，默认为true
+* @param { regExp} 匹配文件的正则表达式，可选
+* */
+
 /*
 *   png: /\.png$/i
 *   jpg jpeg: /\.jpe?g$/i
